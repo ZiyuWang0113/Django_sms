4c5cc5ebd30e2133d1615b7a984f3ab22de322eb
Jerry Wang <ziw003@ucsd.edu>
2024-09-21 16:55:27 -0400

    Update URL

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-s4r=xqftli(c^dr%aw4=20!05$ljmjn)ycg%l59$#mo2w8pbnt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
---
+++ sales/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # 不带一级路由表
+    path('orders/', views.listorders),
+]
