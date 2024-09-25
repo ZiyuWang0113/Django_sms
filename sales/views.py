ef2242c2d331a3dbe0d72518f93e59800a40e0f6
Jerry Wang <ziw003@ucsd.edu>
2024-09-24 20:57:59 -0400

    Update
    update django template

--- sales/views.py
+++ sales/views.py
@@ -2,29 +2,70 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # 导入 Customer 对象定义
 from common.models import Customer
+from django.template import engines
 
 
 def listorders(request):
     return HttpResponse("下面是系统中所有的订单信息啊啊啊。。。")
 
 
+# 先定义好HTML模板
+html_template = '''
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<style>
+table {
+    border-collapse: collapse;
+}
+th, td {
+    padding: 8px;
+    text-align: left;
+    border-bottom: 1px solid #ddd;
+}
+</style>
+</head>
+    <body>
+        <table>
+        <tr>
+        <th>id</th>
+        <th>姓名</th>
+        <th>电话号码</th>
+        <th>地址</th>
+        </tr>
+
+        {% for customer in customers %}
+            <tr>
+
+            {% for name, value in customer.items %}            
+                <td>{{ value }}</td>            
+            {% endfor %}
+
+            </tr>
+        {% endfor %}
+
+        </table>
+    </body>
+</html>
+'''
+
+django_engine = engines['django']
+template = django_engine.from_string(html_template)
+
+
 def listcustomers(request):
     # 返回一个 QuerySet 对象 ，包含所有的表记录
-    # 每条表记录都是是一个dict对象，
-    # key 是字段名，value 是 字段值
     qs = Customer.objects.values()
-    # 检查url中是否有参数 phonenumber, 如果没有则return None
+
+    # 检查url中是否有参数phone
     ph = request.GET.get('phone', None)
+
     # 如果有，添加过滤条件
     if ph:
         qs = qs.filter(phone=ph)
 
-    # 定义返回字符串
-    retStr = ''
-    for customer in qs:
-        for name, value in customer.items():
-            retStr += f'{name} : {value} | '
-        # <br> 表示换行
-        retStr += '<br>'
+    # 传入渲染模板需要的参数
+    rendered = template.render({'customers': qs})
 
-    return HttpResponse(retStr)
+    return HttpResponse(rendered)
